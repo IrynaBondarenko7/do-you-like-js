ca60877fab2f39901734a295eee2fe659e9e8720
Iryna Bondarenko <ira.bondarenko@ukr.net>
2023-03-23 19:59:05 +0000

    added bootstrap libary

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const btn = document.querySelector(".btn");
+const btnAgree = document.querySelector(".btn-agree");
+const modalLabel = document.querySelector("#exampleModalLabel");
+const modalText = document.querySelector(".modal-text");
+const modalImg = document.querySelector(".modal-img");
 
 const onBtnHover = (event) => {
+  btn.style.position = "absolute";
   const { target: button } = event;
 
   button.style.top = `${Math.floor(
@@ -16,3 +21,27 @@ function onClick(event) {
 }
 btn.addEventListener("mouseover", onBtnHover);
 btn.addEventListener("click", onClick);
+
+btnAgree.addEventListener("shown.bs.modal", () => {
+  myInput.focus();
+});
+
+var myModal = new BSN.Modal("#exampleModal");
+
+btnAgree.addEventListener("click", onBtnAgreeClick);
+function onBtnAgreeClick() {
+  modalLabel.textContent = "Congratulations!";
+  modalText.textContent = "You made the right choice!";
+  modalImg.src =
+    "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMWZiM2QyNDg2YTg1ZmU1ZGIwMDUxZjc4MjA1NDI2MzBjZmViNWQ5MiZjdD1n/WtOkaikiwaR87ZvAFH/giphy.gif";
+  myModal.show();
+}
+
+btn.addEventListener("click", onBtnDisagreeClick);
+
+function onBtnDisagreeClick() {
+  modalLabel.textContent = "Oh, it's so sad(";
+  modalText.textContent = "Maybe you'll give Java Script another chance?";
+  modalImg.src = "https://thumbs.gfycat.com/HotGrossApe-size_restricted.gif";
+  myModal.show();
+}
